3bf78e91f989da0a64c20c3196bf0b616ab12186
FarazAhmad2 <farazahmad574@gmail.com>
2024-07-08 22:50:26 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,7 @@ import Tooltip from "./Tooltip";
 const App = () => {
   return (
     <div>
-      <Tooltip text={'This is a tooltip'}>
-        <h1>Hover over me</h1>
-      </Tooltip>
-      <Tooltip text={'This is another tooltip'}>
-        <p>Hover on me to see another tooltip</p>
-      </Tooltip>
+      <Tooltip text={'This is a tooltip'} text2={'This is another tooltip'} />
     </div>
   );
 };
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { useState } from "react";
-import '../styles/App.css'
+import "../styles/App.css";
 
 const Tooltip = (props) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [Visible, setVisible] = useState(false);
 
   return (
-    <div className="tooltip"
-      onMouseEnter={() => setIsVisible(true)}
-      onMouseLeave={() => setIsVisible(false)}
-    >
-      {props.children}
+    <div className="tooltip">
+      
+      <h2 className="tooltiptext" style={{ visibility: isVisible ? "visible" : "hidden" }}>{props.text}</h2>
+      <h1
+        onMouseEnter={() => setIsVisible(true)}
+        onMouseLeave={() => setIsVisible(false)}
+      >
+        Hover over me
+      </h1>
       <p
         className="tooltiptext"
-        style={{ visibility: isVisible ? "visible" : "hidden" }}
+        style={{ visibility: Visible ? "visible" : "hidden" }}
       >
-        {props.text}
+        {props.text2}
+      </p>
+      <p
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+      >
+        Hover over me to see another tooltip
       </p>
     </div>
   );
